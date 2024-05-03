93e9f388440955b18b8064e43fd25f47db2379af
Anthony HAMON <hamon.anth@gmail.com>
2024-05-03 22:09:18 +0200

    Add tests on cmd package

---
+++ cmd/edit_test.go
@@ -0,0 +1,80 @@
+package cmd
+
+import (
+	"bytes"
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewEditCmd(t *testing.T) {
+	type scenario struct {
+		name  string
+		setup func(*testing.T) (workspaceManager, []string)
+		test  func(*testing.T, error)
+	}
+	scenarios := []scenario{
+		{
+			"An error occurred when editing a workspace",
+			func(t *testing.T) (workspaceManager, []string) {
+				w := newMockWorkspaceManager(t)
+				args := []string{"api"}
+				w.Mock.On("Edit", args[0]).Return(errors.New("an error occurred"))
+				return w, args
+			},
+			func(t *testing.T, err error) {
+				assert.Error(t, err)
+			},
+		},
+		{
+			"An error occurred when editing a workspace env",
+			func(t *testing.T) (workspaceManager, []string) {
+				w := newMockWorkspaceManager(t)
+				args := []string{"api", "prod"}
+				w.Mock.On("EditEnv", args[0], args[1]).Return(errors.New("an error occurred"))
+				return w, args
+			},
+			func(t *testing.T, err error) {
+				assert.Error(t, err)
+			},
+		},
+		{
+			"Editing a workspace successfully",
+			func(t *testing.T) (workspaceManager, []string) {
+				w := newMockWorkspaceManager(t)
+				args := []string{"api"}
+				w.Mock.On("Edit", args[0]).Return(nil)
+				return w, args
+			},
+			func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
+		{
+			"Editing a workspace env successfully",
+			func(t *testing.T) (workspaceManager, []string) {
+				w := newMockWorkspaceManager(t)
+				args := []string{"api", "prod"}
+				w.Mock.On("EditEnv", args[0], args[1]).Return(nil)
+				return w, args
+			},
+			func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
+	}
+	for _, s := range scenarios {
+		t.Run(s.name, func(t *testing.T) {
+			os.Setenv("EDITOR", "emacs")
+			os.Setenv("SHELL", "/bin/sh")
+			w, args := s.setup(t)
+			cmd := newEditCmd(w)
+			cmd.SetArgs(args)
+			cmd.SetErr(&bytes.Buffer{})
+			cmd.SetOut(&bytes.Buffer{})
+			s.test(t, cmd.Execute())
+		})
+	}
+}
